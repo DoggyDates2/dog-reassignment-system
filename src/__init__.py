b611d9f79c87509ae24fdfd6003f612dbdee06a3
DoggyDates2 <123763935+DoggyDates2@users.noreply.github.com>
2025-06-30 06:53:04 -0400

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,25 @@
+"""
+Optimized Dog Reassignment System
+Source package initialization
+"""
+
+__version__ = "1.0.0"
+__author__ = "Your Name"
+__description__ = "Multi-algorithm optimization system for dog assignment logistics"
+
+# Import main components for easy access
+try:
+    from .data_manager import DataManager, DogProfile, DriverProfile, DistanceMatrix, create_data_manager
+    from .optimization_engine import MultiAlgorithmOptimizer, AssignmentSolution
+    from .assignment_logic import AssignmentEngine, ReassignmentResult, create_assignment_engine
+    from .ui_components import SessionState, DashboardUI
+
+    __all__ = [
+        'DataManager', 'DogProfile', 'DriverProfile', 'DistanceMatrix', 'create_data_manager',
+        'MultiAlgorithmOptimizer', 'AssignmentSolution',
+        'AssignmentEngine', 'ReassignmentResult', 'create_assignment_engine',
+        'SessionState', 'DashboardUI'
+    ]
+except ImportError:
+    # Files not uploaded yet, that's OK
+    pass
